87be6ae3ea6c85ab879d2efc24b69247c3448b48
Jaromir Chaloupka <jaromir.chaloupka@barum.cz>
2024-04-19 08:44:15 +0200

    Lesson 1 + Lesson 2

---
+++ CzechitasCsharp2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CzechitasCsharp2;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.Write("Zadej den odjezdu na dovolenou: ");
+        string day = Console.ReadLine();
+
+        Console.Write("Zadej měsíc odjezdu na dovolenou: ");
+        string month = Console.ReadLine();
+
+        Console.Write("Zadej rok odjezdu na dovolenou: ");
+        string year = Console.ReadLine();
+
+        if (!int.TryParse(year, out int intYear) || intYear > 3000)
+        {
+            Console.Write("Zadej správně rok odjezdu na dovolenou: ");
+            year = Console.ReadLine();
+        }
+
+        DateTime vacationsStart = new DateTime(int.Parse(year), int.Parse(month), int.Parse(day));
+
+        TimeSpan timeToVacations = vacationsStart - DateTime.Now;
+
+        if (timeToVacations < TimeSpan.Zero)
+        {
+            Console.WriteLine("Tuto dovolenou už máš za sebou.");
+        }
+        else if (timeToVacations < TimeSpan.FromDays(1))
+        {
+            Console.WriteLine("Nevysedávej u počítače a bež si balit kurfy, za chvíli odjíždíš!");
+        }
+        else
+        {
+            Console.WriteLine($"Počet dní do začátku dovolené: {timeToVacations.Days}");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
---
+++ L02_Exceptions/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace L02_Exceptions;
+
+public class Clovek
+{
+    public string Jmeno;
+    public int TelCislo;
+
+    public Clovek(string jmeno, int telCislo)
+    {
+        if (string.IsNullOrEmpty(jmeno))
+        {
+            throw new ArgumentNullException(nameof(Jmeno), "Jmeno musi byt vyplneno.");
+        }
+
+        if (telCislo.ToString().Length != 9)
+        {
+            throw new ArgumentException("Telefonni cislo musí mít 9 čísel.", nameof(TelCislo));
+        }
+
+        Jmeno = jmeno;
+        TelCislo = telCislo;
+    }
+
+    public string VypisJmenoACislo()
+    {
+        return $"{Jmeno}: {TelCislo}";
+    }
+}
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Program na vytvareni telefonniho seznamu");
+        Console.WriteLine("========================================");
+        Console.Write("Zadejte jmeno: ");
+        string jmeno = Console.ReadLine();
+        Console.Write("Zadejte telefonni cislo: ");
+        string cislo = Console.ReadLine();
+
+        try
+        {
+            Clovek prvniClovek = new Clovek(jmeno, int.Parse(cislo));
+            Console.WriteLine(prvniClovek.VypisJmenoACislo());
+        }
+        catch (ArgumentException argEx)
+        {
+            Console.WriteLine($"Chyba zadanych udaju: {argEx.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Neznama chyba: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("Vkladani telefonniho cisla bylo dokonceno.");
+        }
+
+        Console.ReadLine();
+    }
+}
